ef2ee7f67e426d2ff00b9511c949e4b4b57c1b0a
aleksandrayakovleva132 <33125810+aleksandrayakovleva132@users.noreply.github.com>
2021-03-16 18:33:30 +0200

    meetup radio-list

--- 01-basics/03-MeetupTitle/script.js
+++ 01-basics/03-MeetupTitle/script.js
@@ -1,3 +1,28 @@
-// import Vue from './vendor/vue.esm.browser.js';
+import Vue from './vendor/vue.esm.browser.js';
 
-// new Vue();
+const fetchMeetups = () => fetch('../api/meetups.json').then((res) => res.json());
+
+const app = new Vue({
+  data() {
+    return {
+      rawMeetups: [],
+      active: '',
+    };
+  },
+
+  computed: {
+    meetups() {
+      return this.rawMeetups.map((meetup) => ({
+        ...meetup,
+      }));
+    },
+  },
+
+  mounted() {
+    fetchMeetups().then((meetups) => {
+      this.rawMeetups = meetups.slice(0, 5);
+    });
+  },
+});
+
+app.$mount('#app');
