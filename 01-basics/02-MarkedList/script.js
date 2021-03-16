dd83bb5778b23c937ceff407b70beb0c58abaf89
aleksandrayakovleva132 <33125810+aleksandrayakovleva132@users.noreply.github.com>
2021-03-16 16:40:42 +0200

    search list

--- 01-basics/02-MarkedList/script.js
+++ 01-basics/02-MarkedList/script.js
@@ -1,7 +1,7 @@
-// import Vue from './vendor/vue.esm.browser.js';
+import Vue from './vendor/vue.esm.browser.js';
 
 // From https://jsonplaceholder.typicode.com/comments
-/*
+
 const emails = [
   'Eliseo@gardner.biz',
   'Jayne_Kuhic@sydney.com',
@@ -29,6 +29,14 @@ const emails = [
   'Jeffery@juwan.us',
   'Isaias_Kuhic@jarrett.net',
 ];
-*/
 
-// new Vue();
+const app = new Vue({
+  data() {
+    return {
+      emailsList: emails,
+      search: null,
+    };
+  },
+});
+
+app.$mount('#app');
