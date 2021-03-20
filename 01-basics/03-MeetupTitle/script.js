d0808fe045499a48e64ffc4e29022efed5b244fe
aleksandrayakovleva132 <33125810+aleksandrayakovleva132@users.noreply.github.com>
2021-03-20 18:56:38 +0200

    save

--- 01-basics/03-MeetupTitle/script.js
+++ 01-basics/03-MeetupTitle/script.js
@@ -1,26 +1,38 @@
 import Vue from './vendor/vue.esm.browser.js';
 
-const fetchMeetups = () => fetch('../api/meetups.json').then((res) => res.json());
+function fetchMeetup(id) {
+  return fetch(`https://course-vue.javascript.ru/api/meetups/${id}`).then((res) => res.json());
+}
 
 const app = new Vue({
   data() {
     return {
-      rawMeetups: [],
-      active: '',
+      rawMeetup: [],
+      id: 3,
     };
   },
 
   computed: {
-    meetups() {
-      return this.rawMeetups.map((meetup) => ({
-        ...meetup,
-      }));
+    meetup() {
+      if (!this.rawMeetup) {
+        return null;
+      }
+      return {
+        ...this.rawMeetup,
+        id: this.rawMeetup = 5,
+      };
     },
   },
 
+  methods: {
+    getId () {
+      return this.id = this.rawMeetup.id;
+    }
+  },
+
   mounted() {
-    fetchMeetups().then((meetups) => {
-      this.rawMeetups = meetups.slice(0, 5);
+    fetchMeetup(this.id).then((meetup) => {
+      this.rawMeetup = meetup;
     });
   },
 });
