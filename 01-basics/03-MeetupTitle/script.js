d6349fe616379f7e0789516f4fd4ead657593299
aleksandrayakovleva132 <33125810+aleksandrayakovleva132@users.noreply.github.com>
2021-03-23 23:37:51 +0200

    solution-01-03 without another test

--- 01-basics/03-MeetupTitle/script.js
+++ 01-basics/03-MeetupTitle/script.js
@@ -1,3 +1,22 @@
-// import Vue from './vendor/vue.esm.browser.js';
+import Vue from './vendor/vue.esm.browser.js';
 
-// new Vue();
+const app = new Vue({
+  data() {
+    return {
+      activeId: 0,
+      rawMeetup: null,
+    };
+  },
+
+  watch: {
+    activeId() {
+      return fetch(`https://course-vue.javascript.ru/api/meetups/${this.activeId}`)
+        .then((res) => res.json())
+        .then((meetup) => {
+          this.rawMeetup = meetup.title;
+        });
+    },
+  },
+});
+
+app.$mount('#app');
